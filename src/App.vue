e60637de416b307c6bce088b43ce0b13485a125d
zhiss <1002759499@qq.com>
2020-01-09 11:10:20 +0800

    fix:第一次

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="content">
+        Test111
+        <Test></Test>
+    </div>
+    
+</template>
+
+<script>
+import Test from './view/test'
+export default {
+    name:'app',
+    components:{
+        Test,
+    }
+}
+</script>
+
+<style scoped lang='scss'>
+.content{
+    color: green;
+    height: 40px;
+    h1{
+        color: red;
+    }
+}
+
+</style>
---
+++ src/view/test.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="test">
+       <h1>啧啧</h1>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang='scss'>
+    h1{
+        color: green;
+    }
+</style>
